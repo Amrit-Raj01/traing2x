fd070769cc5cc61c9664c02fb55c58201aa13109
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-06-08 16:07:57 +0530

    Update and rename config.yml to Song.java

---
+++ Song.java
@@ -0,0 +1,8 @@
+package model;
+public class Song {
+    private String id, title, category, url, img;
+    public Song(String id, String title, String cat, String url, String img) {
+        this.id=id; this.title=title; this.category=cat; this.url=url; this.img=img;
+    }
+    // getters/setters omitted for brevity
+}
